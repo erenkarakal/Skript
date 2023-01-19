6f75651cdaf631df5d78c1cd8e5ce92d6748d39c
Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
2023-01-19 14:19:13 -0600

    Add comments explaining why the proxy is used

--- src/main/java/ch/njol/skript/expressions/ExprInventory.java
+++ src/main/java/ch/njol/skript/expressions/ExprInventory.java
@@ -92,10 +92,13 @@ public class ExprInventory extends SimpleExpression<Object> {
 					BlockState state = ((BlockStateMeta) meta).getBlockState();
 					if (state instanceof Container) {
 						Inventory underlyingInv = ((Container) state).getInventory();
-						Inventory proxy = (Inventory) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{Inventory.class}, new InvocationHandler() {
+						// The proxy is used here to ensure that any changes to the inventory are reflected in the
+						// BlockStateMeta and ItemMeta of `holder`
+						Inventory proxy = (Inventory) Proxy.newProxyInstance(getClass().getClassLoader(), new Class[]{Inventory.class}, new InvocationHandler() {
 							@Override
 							public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 								Object returnValue = method.invoke(underlyingInv, args);
+								// calling update here causes the changes to the inventory to be synced to the meta
 								boolean updateSucceeded = state.update();
 								if (updateSucceeded) {
 									((BlockStateMeta) meta).setBlockState(state);
