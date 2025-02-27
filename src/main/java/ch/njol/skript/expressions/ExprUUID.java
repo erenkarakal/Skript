4179b14de19477617c86243741c7615398141232
Eren KARA <karaeren362@gmail.com>
2025-02-27 12:38:57 +0300

    cleanup ExprUUID

--- src/main/java/ch/njol/skript/expressions/ExprUUID.java
+++ src/main/java/ch/njol/skript/expressions/ExprUUID.java
@@ -17,10 +17,7 @@ import ch.njol.skript.expressions.base.SimplePropertyExpression;
  * @author Peter Güttinger
  */
 @Name("UUID")
-@Description({"The UUID of a player, entity or world.",
-		"In the future there will be an option to use a player's UUID instead of the name in variable names (i.e. when %player% is used), but for now this can be used.",
-		"<em>Please note that this expression does not work for offline players if you are under 1.8!</em>"}) 
-// TODO [UUID] update documentation after release. Add note about requiring Bukkit 1.7.(9/10)?
+@Description({"The UUID of a player, entity or world."})
 @Examples({"# prevents people from joining the server if they use the name of a player",
 		"# who has played on this server at least once since this script has been added",
 		"on login:",
@@ -38,10 +35,10 @@ public class ExprUUID extends SimplePropertyExpression<Object, String> {
 
 	@Override
 	@Nullable
-	public String convert(final Object o) {
-		if (o instanceof OfflinePlayer) {
+	public String convert(final Object object) {
+		if (object instanceof OfflinePlayer offlinePlayer) {
 			try {
-				return ((OfflinePlayer) o).getUniqueId().toString();
+				return offlinePlayer.getUniqueId().toString();
 			} catch (UnsupportedOperationException e) {
 				// Some plugins (ProtocolLib) try to emulate offline players, but fail miserably
 				// They will throw this exception... and somehow server may freeze when this happens
@@ -49,10 +46,10 @@ public class ExprUUID extends SimplePropertyExpression<Object, String> {
 				e.printStackTrace();
 				return null;
 			}
-		} else if (o instanceof Entity) {
-			return ((Entity)o).getUniqueId().toString();
-		} else if (o instanceof World) {
-			return ((World) o).getUID().toString();
+		} else if (object instanceof Entity entity) {
+			return entity.getUniqueId().toString();
+		} else if (object instanceof World world) {
+			return world.getUID().toString();
 		}
 		return null;
 	}
