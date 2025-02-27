7a2e97c4dc30c861c4bfae22f55b0c68447693bf
Eren KARA <karaeren362@gmail.com>
2025-02-27 12:42:43 +0300

    cleanup CondHasResourcePack

--- src/main/java/ch/njol/skript/conditions/CondHasResourcePack.java
+++ src/main/java/ch/njol/skript/conditions/CondHasResourcePack.java
@@ -2,12 +2,10 @@ package ch.njol.skript.conditions;
 
 import org.bukkit.entity.Player;
 
-import ch.njol.skript.Skript;
 import ch.njol.skript.conditions.base.PropertyCondition;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.RequiredPlugins;
 import ch.njol.skript.doc.Since;
 
 @Name("Has Resource Pack")
@@ -15,12 +13,10 @@ import ch.njol.skript.doc.Since;
 		"player's own resource pack, only the resource pack that sent by the server.")
 @Examples("if the player has a resource pack loaded:")
 @Since("2.4")
-@RequiredPlugins("Paper 1.9 or newer")
 public class CondHasResourcePack extends PropertyCondition<Player> {
 
 	static {
-		if (Skript.methodExists(Player.class, "hasResourcePack"))
-			register(CondHasResourcePack.class, PropertyType.HAVE, "[a] resource pack [(loaded|installed)]", "players");
+		register(CondHasResourcePack.class, PropertyType.HAVE, "[a] resource pack [(loaded|installed)]", "players");
 	}
 
 	@Override
