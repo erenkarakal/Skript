d62d417fa299ad7548a7a96d96a36ed5045ba9d0
Eren KARA <karaeren362@gmail.com>
2025-06-04 21:53:53 +0300

    add ExprDateInTimezone

---
+++ src/main/java/ch/njol/skript/conditions/CondIsTimezoneValid.java
@@ -0,0 +1,52 @@
+package ch.njol.skript.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.DateTimeException;
+import java.time.ZoneId;
+
+public class CondIsTimezoneValid extends Condition {
+
+	static {
+		Skript.registerCondition(CondIsTimezoneValid.class, "time[ ]zone[s] %strings% (is|are) [negate:in]valid");
+	}
+
+	private Expression<String> timezones;
+	private boolean isNegated;
+
+	@Override
+	public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+		timezones = (Expression<String>) expressions[0];
+		isNegated = parseResult.hasTag("negate");
+		return true;
+	}
+
+	@Override
+	public boolean check(Event event) {
+		for (String timezone : timezones.getAll(event)) {
+			if (timezone == null) {
+				return isNegated;
+			}
+
+			try {
+				ZoneId.of(timezone);
+			} catch (DateTimeException e) {
+				return isNegated;
+			}
+		}
+
+		return !isNegated;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		return "timezone " + timezones.toString(event, debug) + " is " + (isNegated ? "in" : "") + "valid";
+	}
+
+}
