ce306e79c0d56ba02704f4ffb5ea8c0e528e4397
Eren KARA <karaeren362@gmail.com>
2025-06-04 21:15:01 +0300

    revert ExprNow

--- src/main/java/ch/njol/skript/expressions/ExprNow.java
+++ src/main/java/ch/njol/skript/expressions/ExprNow.java
@@ -1,10 +1,13 @@
 package ch.njol.skript.expressions;
 
-import ch.njol.skript.doc.*;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
@@ -12,84 +15,38 @@ import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.util.Date;
 import ch.njol.util.Kleenean;
 
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
 @Name("Now")
-@Description({
-	"The current <a href='classes.html#date'>system time</a> of the server. "
-		+ "Use <a href='#ExprTime'>time</a> to get the <a href='classes.html#time'>Minecraft time</a> of a world.",
-	"Optionally specify a timezone to get the current date at a timezone. The returned value might not be equal to"
-		+ "'now' without timezones. If a timezone is invalid no value will be returned.",
-	"Use <a href='#ExprAllTimezones'>all timezones</a> to get a list of valid timezones."
-})
-@Example("broadcast \"Current server time: %now%\"")
-@Example("""
-	set {_date} to now in timezone "Europe/Istanbul"
-	set {_clock} to {_date} formatted as "kk:mm"
-	send "It is currently %{_clock}% in Istanbul!" to player
-	""")
-@Since("1.4, INSERT VERSION (timezones)")
+@Description("The current <a href='classes.html#date'>system time</a> of the server. Use <a href='#ExprTime'>time</a> to get the <a href='classes.html#time'>Minecraft time</a> of a world.")
+@Examples({"broadcast \"Current server time: %now%\""})
+@Since("1.4")
 public class ExprNow extends SimpleExpression<Date> {
-	
+
 	static {
-		Skript.registerExpression(ExprNow.class, Date.class, ExpressionType.SIMPLE, "now [timezone:in time[ ]zone %-string%]");
+		Skript.registerExpression(ExprNow.class, Date.class, ExpressionType.SIMPLE, "now");
 	}
 
-	private boolean usingTimezone;
-	private Expression<String> timezone;
-	
 	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		usingTimezone = parseResult.hasTag("timezone");
-		timezone = (Expression<String>) exprs[0];
+	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
 		return true;
 	}
-	
+
 	@Override
-	protected Date[] get(Event event) {
-		if (usingTimezone) {
-			String timezone = this.timezone.getSingle(event);
-			if (timezone == null) {
-				return new Date[0];
-			}
-
-			ZoneId targetZoneId;
-			try {
-				targetZoneId = ZoneId.of(timezone);
-			} catch (DateTimeException e) { // invalid zone format
-				return new Date[0];
-			}
-
-			ZoneId localZoneId = ZoneId.systemDefault();
-			Instant shiftedNow = ZonedDateTime.now(targetZoneId)
-				.toLocalDateTime()
-				.atZone(localZoneId)
-				.toInstant();
-			java.util.Date javaDate = java.util.Date.from(shiftedNow);
-			Date date = Date.fromJavaDate(javaDate);
-			return new Date[]{ date };
- 		}
-		return new Date[]{ new Date() };
+	protected Date[] get(final Event e) {
+		return new Date[] {new Date()};
 	}
-	
+
 	@Override
 	public boolean isSingle() {
 		return true;
 	}
-	
+
 	@Override
 	public Class<? extends Date> getReturnType() {
 		return Date.class;
 	}
-	
+
 	@Override
-	public String toString(@Nullable Event event, boolean debug) {
-		if (usingTimezone) {
-			return "now in timezone " + timezone.toString(event, debug);
-		}
+	public String toString(final @Nullable Event e, final boolean debug) {
 		return "now";
 	}
 
