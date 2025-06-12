4da9776da4422760c994c67046659f86d569e6a0
Eren KARA <karaeren362@gmail.com>
2025-06-12 18:14:55 +0300

    update ExprGameRule

--- src/main/java/ch/njol/skript/expressions/ExprGameRule.java
+++ src/main/java/ch/njol/skript/expressions/ExprGameRule.java
@@ -10,7 +10,6 @@ import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.RequiredPlugins;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
@@ -18,74 +17,82 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.util.GameruleValue;
 import ch.njol.util.Kleenean;
-import ch.njol.util.coll.CollectionUtils;
 
 @Name("Gamerule Value")
 @Description("The gamerule value of a world.")
 @Examples({"set the gamerule commandBlockOutput of world \"world\" to false"})
 @Since("2.5")
-@RequiredPlugins("Minecraft 1.13+")
 public class ExprGameRule extends SimpleExpression<GameruleValue> {
 	
 	static {
-		if (Skript.classExists("org.bukkit.GameRule")) {
-			Skript.registerExpression(ExprGameRule.class, GameruleValue.class, ExpressionType.COMBINED,
-				"[the] gamerule %gamerule% of %worlds%");
-		}
+		Skript.registerExpression(ExprGameRule.class, GameruleValue.class, ExpressionType.COMBINED, "[the] gamerule %gamerule% of %worlds%");
 	}
-	
-	@SuppressWarnings("null")
+
+	private boolean isSingleWorld;
+
 	private Expression<GameRule> gamerule;
-	@SuppressWarnings("null")
-	private Expression<World> world;
+	private Expression<World> worlds;
 	
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		gamerule = (Expression<GameRule>) exprs[0];
-		world = (Expression<World>) exprs[1];
+		worlds = (Expression<World>) exprs[1];
+		isSingleWorld = worlds.isSingle();
 		return true;
 	}
 	
 	@Override
 	@Nullable
 	public Class<?>[] acceptChange(final ChangeMode mode) {
-		if (mode == ChangeMode.SET) 
-            return CollectionUtils.array(Boolean.class, Number.class);
+		if (mode == ChangeMode.SET) {
+			return new Class[]{Boolean.class, Number.class};
+		}
 		return null;
 	}
 	
 	@Override
 	public void change(final Event e, final @Nullable Object[] delta, final ChangeMode mode) {
-		assert delta != null;
 		if (mode == ChangeMode.SET) {
-			GameRule bukkitGamerule = gamerule.getSingle(e);
-			if (bukkitGamerule == null) 
-                return;
-			for (World gameruleWorld : world.getArray(e))
-                gameruleWorld.setGameRule(bukkitGamerule, delta[0]);
+			GameRule gamerule = this.gamerule.getSingle(e);
+			if (gamerule == null) {
+				return;
+			}
+
+			Object value = delta[0];
+			if (value instanceof Number number && gamerule.getType().equals(Integer.class)) {
+				value = number.intValue();
+			}
+
+			for (World gameruleWorld : worlds.getArray(e)) {
+                gameruleWorld.setGameRule(gamerule, value);
+			}
 		}
 	}
 		
 	@Nullable
 	@Override
 	protected GameruleValue[] get(Event e) {
-		GameRule<?> bukkitGamerule = gamerule.getSingle(e);
-		if (bukkitGamerule == null) 
-            return null;
-		World[] gameruleWorlds = world.getArray(e);
-		GameruleValue[] gameruleValues = new GameruleValue[gameruleWorlds.length];
+		GameRule<?> gamerule = this.gamerule.getSingle(e);
+		if (gamerule == null) {
+			return null;
+		}
+
+		World[] worlds = this.worlds.getArray(e);
+		GameruleValue[] gameruleValues = new GameruleValue[worlds.length];
 		int index = 0;
-		for (World gameruleWorld : gameruleWorlds) {
-			Object gameruleValue = gameruleWorld.getGameRuleValue(bukkitGamerule);
+
+		for (World world : worlds) {
+			Object gameruleValue = world.getGameRuleValue(gamerule);
 			assert gameruleValue != null;
 			gameruleValues[index++] = new GameruleValue<>(gameruleValue);
 		}
+
 		return gameruleValues;
 	}
 	
 	@Override
 	public boolean isSingle() {
-		return false;
+		return isSingleWorld;
 	}
 	
 	@Override
@@ -95,6 +102,7 @@ public class ExprGameRule extends SimpleExpression<GameruleValue> {
 	
 	@Override
 	public String toString(@Nullable Event e, boolean debug) {
-		return "the gamerule value of " + gamerule.toString(e, debug) + " for world " + world.toString(e, debug);
+		return "the gamerule " + gamerule.toString(e, debug) + " of worlds " + worlds.toString(e, debug);
 	}
+
 }
