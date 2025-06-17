a4f28684d335ac6f886e5248afdfce424b1c2266
Eren KARA <karaeren362@gmail.com>
2025-06-17 13:21:38 +0300

    simpleliteral

--- src/main/java/ch/njol/skript/expressions/ExprAllTimezones.java
+++ src/main/java/ch/njol/skript/expressions/ExprAllTimezones.java
@@ -8,7 +8,7 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.lang.util.SimpleLiteral;
 import ch.njol.util.Kleenean;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
@@ -19,7 +19,7 @@ import java.time.ZoneId;
 @Description("Returns a list of all timezones that can be used in the <a href='#ExprDateInTimezone'>date in timezone</a> expression.")
 @Example("set {_timezones::*} to all timezones")
 @Since("INSERT VERSION")
-public class ExprAllTimezones extends SimpleExpression<String> {
+public class ExprAllTimezones extends SimpleLiteral<String> {
 
 	static {
 		Skript.registerExpression(ExprAllTimezones.class, String.class, ExpressionType.SIMPLE, "all [of [the]] time[ ]zones");
@@ -27,24 +27,13 @@ public class ExprAllTimezones extends SimpleExpression<String> {
 
 	private static String[] timezones = ZoneId.getAvailableZoneIds().toArray(new String[0]);
 
+	public ExprAllTimezones() {
+		super(timezones, String.class, true);
+	}
+
 	@Override
-	public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		return true;
-	}
-
-	@Override
-	protected String @Nullable [] get(Event event) {
-		return timezones;
-	}
-
-	@Override
-	public boolean isSingle() {
-		return false;
-	}
-
-	@Override
-	public Class<? extends String> getReturnType() {
-		return String.class;
 	}
 
 	@Override
