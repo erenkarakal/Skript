a9be31df586d6d1f667d4362e8a690c929d64a8a
Eren KARA <karaeren362@gmail.com>
2025-06-21 01:13:16 +0300

    update ExprAmount

--- src/main/java/ch/njol/skript/expressions/ExprAmount.java
+++ src/main/java/ch/njol/skript/expressions/ExprAmount.java
@@ -6,13 +6,11 @@ import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.ExpressionList;
-import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.*;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.Variable;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.lang.util.common.AnyAmount;
+import ch.njol.skript.util.LiteralUtils;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.event.Event;
@@ -22,31 +20,31 @@ import java.util.Map;
 
 @Name("Amount")
 @Description({"The amount or size of something.",
-		"Please note that <code>amount of %items%</code> will not return the number of items, but the number of stacks, e.g. 1 for a stack of 64 torches. To get the amount of items in a stack, see the <a href='#ExprItemAmount'>item amount</a> expression.",
-		"",
-		"Also, you can get the recursive size of a list, which will return the recursive size of the list with sublists included, e.g.",
-		"",
-		"<pre>",
-		"{list::*} Structure<br>",
-		"  ├──── {list::1}: 1<br>",
-		"  ├──── {list::2}: 2<br>",
-		"  │     ├──── {list::2::1}: 3<br>",
-		"  │     │    └──── {list::2::1::1}: 4<br>",
-		"  │     └──── {list::2::2}: 5<br>",
-		"  └──── {list::3}: 6",
-		"</pre>",
-		"",
-		"Where using %size of {list::*}% will only return 3 (the first layer of indices only), while %recursive size of {list::*}% will return 6 (the entire list)",
-		"Please note that getting a list's recursive size can cause lag if the list is large, so only use this expression if you need to!"})
+	"Please note that <code>amount of %items%</code> will not return the number of items, but the number of stacks, e.g. 1 for a stack of 64 torches. To get the amount of items in a stack, see the <a href='#ExprItemAmount'>item amount</a> expression.",
+	"",
+	"Also, you can get the recursive size of a list, which will return the recursive size of the list with sublists included, e.g.",
+	"",
+	"<pre>",
+	"{list::*} Structure<br>",
+	"  ├──── {list::1}: 1<br>",
+	"  ├──── {list::2}: 2<br>",
+	"  │     ├──── {list::2::1}: 3<br>",
+	"  │     │    └──── {list::2::1::1}: 4<br>",
+	"  │     └──── {list::2::2}: 5<br>",
+	"  └──── {list::3}: 6",
+	"</pre>",
+	"",
+	"Where using %size of {list::*}% will only return 3 (the first layer of indices only), while %recursive size of {list::*}% will return 6 (the entire list)",
+	"Please note that getting a list's recursive size can cause lag if the list is large, so only use this expression if you need to!"})
 @Examples({"message \"There are %number of all players% players online!\""})
 @Since("1.0")
 public class ExprAmount extends SimpleExpression<Number> {
 
 	static {
 		Skript.registerExpression(ExprAmount.class, Number.class, ExpressionType.PROPERTY,
-				"[the] (amount|number|size) of %numbered%",
-				"[the] (amount|number|size) of %objects%",
-				"[the] recursive (amount|number|size) of %objects%");
+			"[the] (amount|number|size) of %numbered%",
+			"[the] (amount|number|size) of %objects%",
+			"[the] recursive (amount|number|size) of %objects%");
 	}
 
 	@SuppressWarnings("null")
@@ -62,13 +60,23 @@ public class ExprAmount extends SimpleExpression<Number> {
 			this.any = (Expression<AnyAmount>) exprs[0];
 			return true;
 		}
-		this.exprs = exprs[0] instanceof ExpressionList ? (ExpressionList<?>) exprs[0] : new ExpressionList<>(new Expression<?>[]{exprs[0]}, Object.class, false);
+
+		this.exprs = exprs[0] instanceof ExpressionList exprList
+			? exprList
+			: new ExpressionList<>(new Expression<?>[]{ exprs[0] }, Object.class, false);
+
+		if (this.exprs.isSingle()) {
+			Skript.error("'" + this.exprs.toString(null, false) + "' can only ever have one value at most, thus the 'amount of ...' expression is useless. Use '... exists' instead to find out whether the expression has a value.");
+			return false;
+		}
+
+		this.exprs = (ExpressionList<?>) LiteralUtils.defendExpression(this.exprs);
+		if (!LiteralUtils.canInitSafely(this.exprs)) {
+			return false;
+		}
+
 		this.recursive = matchedPattern == 2;
 		for (Expression<?> expr : this.exprs.getExpressions()) {
-			if (expr.isSingle()) {
-				Skript.error("'" + expr.toString(null, false) + "' can only ever have one value at most, thus the 'amount of ...' expression is useless. Use '... exists' instead to find out whether the expression has a value.");
-				return false;
-			}
 			if (recursive && !(expr instanceof Variable<?>)) {
 				Skript.error("Getting the recursive size of a list only applies to variables, thus the '" + expr.toString(null, false) + "' expression is useless.");
 				return false;
@@ -79,20 +87,20 @@ public class ExprAmount extends SimpleExpression<Number> {
 
 	@Override
 	@SuppressWarnings("unchecked")
-	protected Number[] get(Event e) {
+	protected Number[] get(Event event) {
 		if (any != null)
-			return new Number[] {any.getOptionalSingle(e).orElse(() -> 0).amount()};
+			return new Number[] {any.getOptionalSingle(event).orElse(() -> 0).amount()};
 		if (recursive) {
 			int currentSize = 0;
 			for (Expression<?> expr : exprs.getExpressions()) {
-				Object var = ((Variable<?>) expr).getRaw(e);
+				Object var = ((Variable<?>) expr).getRaw(event);
 				if (var != null) { // Should already be a map
 					currentSize += getRecursiveSize((Map<String, ?>) var);
 				}
 			}
 			return new Long[]{(long) currentSize};
 		}
-		return new Long[]{(long) exprs.getArray(e).length};
+		return new Long[]{(long) exprs.getArray(event).length};
 	}
 
 	@Override
@@ -134,13 +142,20 @@ public class ExprAmount extends SimpleExpression<Number> {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
 	private static int getRecursiveSize(Map<String, ?> map) {
+		return getRecursiveSize(map, true);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static int getRecursiveSize(Map<String, ?> map, boolean skipNull) {
 		int count = 0;
 		for (Map.Entry<String, ?> entry : map.entrySet()) {
+			if (skipNull && entry.getKey() == null)
+				continue; // when getting the recursive size of {a::*}, ignore {a}
+
 			Object value = entry.getValue();
 			if (value instanceof Map)
-				count += getRecursiveSize((Map<String, ?>) value);
+				count += getRecursiveSize((Map<String, ?>) value, false);
 			else
 				count++;
 		}
