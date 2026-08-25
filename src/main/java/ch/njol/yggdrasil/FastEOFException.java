ea9cdbee712128a741a70680e1590dc2ec48bd46
Eren Kara <karaeren362@gmail.com>
2026-08-25 13:29:57 +0300

    FastEOFException javadocs

--- src/main/java/ch/njol/yggdrasil/FastEOFException.java
+++ src/main/java/ch/njol/yggdrasil/FastEOFException.java
@@ -2,6 +2,13 @@ package ch.njol.yggdrasil;
 
 import java.io.EOFException;
 
+/**
+ * An EOFException with an empty {@link #fillInStackTrace()} implementation.
+ * <p>
+ * Yggdrasil uses {@link EOFException} as a breakpoint, however the {@link #fillInStackTrace()} method causes
+ * slowdowns while Skript loads variables. This method is not needed as the exception is only being used as a
+ * breakpoint, not an actual exception.
+ */
 public class FastEOFException extends EOFException {
 
 	public FastEOFException() {
