7d1db84ae5ebdaa8044aa122f267b7c35c8fbdf1
Patrick Miller <apickledwalrus@icloud.com>
2026-08-26 17:32:49 -0400

    Add X/Z support for Chunks (#8809)

--- src/main/java/org/skriptlang/skript/bukkit/BukkitModule.java
+++ src/main/java/org/skriptlang/skript/bukkit/BukkitModule.java
@@ -63,6 +63,7 @@ public class BukkitModule extends HierarchicalAddonModule {
 	@Override
 	protected void initSelf(SkriptAddon addon) {
 		Classes.registerClass(new BlockClassInfo());
+		Classes.registerClass(new ChunkClassInfo());
 		Classes.registerClass(new EntityClassInfo());
 		Classes.registerClass(new InventoryClassInfo());
 		Classes.registerClass(new ItemStackClassInfo());
