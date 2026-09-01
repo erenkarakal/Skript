c77f9d71fd2a51c986f81531c90fd430e8289786
Eren Kara <karaeren362@gmail.com>
2026-09-01 21:56:52 +0300

    convert ExprRawName to PropertyExpression and add EntityData support

--- src/main/java/ch/njol/skript/expressions/ExprRawName.java
+++ src/main/java/ch/njol/skript/expressions/ExprRawName.java
@@ -1,66 +1,70 @@
 package ch.njol.skript.expressions;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.bukkit.event.Event;
-import org.jetbrains.annotations.Nullable;
-
-import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.bukkitutil.EntityUtils;
+import ch.njol.skript.entity.EntityData;
+import ch.njol.skript.expressions.base.PropertyExpression;
+import org.bukkit.event.Event;
+
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Example;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Name("Raw Name")
-@Description("The raw Minecraft material name of the given item. Note that this is not guaranteed to give same results on all servers.")
+@Description("""
+	The raw Minecraft material name of the given item or entity type.
+	This expression may return multiple names for item types, but always returns a single name per entity data.
+	Note that this is not guaranteed to give same results on all servers.
+	""")
 @Example("raw name of tool of player")
-@Since("unknown (2.2)")
-public class ExprRawName extends SimpleExpression<String> {
+@Example("raw name of (type of event-entity)")
+@Since("unknown (2.2), entity types (INSERT VERSION)")
+public class ExprRawName extends PropertyExpression<Object, String> {
 	
 	static {
-		Skript.registerExpression(ExprRawName.class, String.class, ExpressionType.SIMPLE, "(raw|minecraft|vanilla) name[s] of %itemtypes%");
+		register(ExprRawName.class, String.class, "(raw|minecraft|vanilla) name[s]", "itemtypes/entitydatas");
 	}
-	
-	@SuppressWarnings("null")
-	private Expression<ItemType> types;
-	
-	@SuppressWarnings({"unchecked", "null"})
+
 	@Override
-	public boolean init(final Expression<?>[] exprs, final int matchedPattern, final Kleenean isDelayed, final ParseResult parseResult) {
-		this.types = (Expression<ItemType>) exprs[0];
+	public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		setExpr(expressions[0]);
 		return true;
 	}
-	
+
 	@Override
-	@Nullable
-	protected String[] get(final Event e) {
-		return Arrays.stream(types.getAll(e))
-				.map(ItemType::getRawNames)
-				.flatMap(List::stream)
-				.toArray(String[]::new);
+	protected String[] get(Event event, Object[] source) {
+		List<String> result = new ArrayList<>();
+		for (Object object : source) {
+			if (object instanceof ItemType itemType) {
+				result.addAll(itemType.getRawNames());
+			} else if (object instanceof EntityData<?> entityData) {
+				result.add(EntityUtils.toBukkitEntityType(entityData).getKey().asString());
+			}
+		}
+		return result.toArray(new String[0]);
 	}
-	
+
 	@Override
 	public boolean isSingle() {
-		return types.isSingle();
+		return getExpr().isSingle();
 	}
-	
+
 	@Override
 	public Class<? extends String> getReturnType() {
 		return String.class;
 	}
-	
-	@SuppressWarnings("null")
+
 	@Override
-	public String toString(final @Nullable Event e, final boolean debug) {
-		return "minecraft name of " + types.toString(e, debug);
+	public String toString(@Nullable Event event, boolean debug) {
+		return "raw name of " + getExpr().toString(event, debug);
 	}
-	
+
 }
