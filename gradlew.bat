ca8bb8b66a58dfafe28996a1e22fb91c793f11b7
Patrick Miller <apickledwalrus@icloud.com>
2026-09-02 12:39:59 -0400

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -79,4 +79,4 @@ endlocal & "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.grad
 
 :exitWithErrorLevel
 @rem Use "%COMSPEC%" /c exit to allow operators to work properly in scripts
-"%COMSPEC%" /c exit %ERRORLEVEL%
+"%COMSPEC%" /c exit %ERRORLEVEL%
